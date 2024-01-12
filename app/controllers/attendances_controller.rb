554834e4f0c0eb00cb21f53e969ad1c357c02e9b
竹内瑳彩 <tahiti.hawaii.29@gmail.com>
2024-01-12 14:46:28 +0900

    createアクションを追加

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -10,4 +10,18 @@ class AttendancesController < ApplicationController
   def new
     @attendance = Attendance.new
   end
+
+  def create
+    @attendance = Attendance.new(attendance_params)
+    if @attendance.save
+      redirect_to attedance_path(@attendance)
+    else
+      render :new
+    end
+  end
+
+  private
+  def attendance_params
+    params.require(:attendance).permit(:title, :start_time, :end_time, :break_time, :content)
+  end
 end
