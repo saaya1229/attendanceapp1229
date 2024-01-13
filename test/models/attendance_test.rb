65d0363b60ac300269c95ac8067abd05a844880f
竹内瑳彩 <tahiti.hawaii.29@gmail.com>
2024-01-13 17:45:23 +0900

    annotateを導入

--- test/models/attendance_test.rb
+++ test/models/attendance_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: attendances
+#
+#  id         :integer          not null, primary key
+#  break_time :integer
+#  content    :text
+#  end_time   :datetime
+#  start_time :datetime
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require 'test_helper'
 
 class AttendanceTest < ActiveSupport::TestCase
