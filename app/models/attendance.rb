5fadae9b728d6ecb3b6e1bbe3af12b07bd4eb666
竹内瑳彩 <tahiti.hawaii.29@gmail.com>
2024-01-16 13:37:58 +0900

    has_many, belongs_to を記述

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -19,6 +19,8 @@
 class Attendance < ApplicationRecord
   validates :start_time, presence: true
 
+  belongs_to :user
+
   def display_created_at
     I18n.l(self.created_at, format: :default)
   end
