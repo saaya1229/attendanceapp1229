82efa25f5b292a81d5c251f18691c4156c02e582
竹内瑳彩 <tahiti.hawaii.29@gmail.com>
2024-01-16 16:24:10 +0900

    delegateを使用

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,20 +25,15 @@ class User < ApplicationRecord
   has_many :attendances, dependent: :destroy
   has_one :profile, dependent: :destroy
 
+
+  delegate :birthday, :gender, to: :profile, allow_nil: true
+
   def has_written?(attendance)
     attendances.exists?(id: attendance.id)
   end
 
   def display_name
     profile&.nickname || self.email.split('@').first
-  end
-
-  def birthday
-    profile&.birthday
-  end
-
-  def gender
-    profile&.gender
   end
 
   def prepare_profile
